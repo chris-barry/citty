54a2a4b95139d328c2a70f43f9ef4944f57af28d
Chris Barry <chris@barry.im>
2015-09-19 13:28:10 -0400

    Refactor weather to be it's own class.

--- panel.py
+++ panel.py
@@ -5,7 +5,6 @@
 # Author: Chris Barry <chris@barry.im>
 # License:  Creative Commons Zero
 
-
 import time
 import urllib.request, urllib.parse, urllib.error
 import curses
@@ -15,6 +14,7 @@ import json
 class CacheFetcher:
 	def __init__(self):
 		self.cache = {}
+
 	def fetch(self, url, max_age=0):
 		#if self.cache.has_key(url):
 		if url in self.cache:
@@ -24,6 +24,21 @@ class CacheFetcher:
 		data = urllib.request.urlopen(url).read()
 		self.cache[url] = (time.time(), data)
 		return data
+
+# Holds configuration and code for fetching weather
+class Weather:
+	def __init__(self, city='', metric=False, cache=60*60):
+		if metric:
+			u = 'c'
+		else:
+			u = 'f'
+		self.WEATHER_WAIT = cache
+		self.WEATHER_URL = 'https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20weather.forecast%20where%20woeid%20in%20(select%20woeid%20from%20geo.places(1)%20where%20text%3D%22{}%22)%20and%20u%3D%22{}%22&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys'.format(urllib.parse.quote(city), u)
+		self.fetcher = CacheFetcher()
+
+	def get(self):
+		response = self.fetcher.fetch(self.WEATHER_URL, self.WEATHER_WAIT).decode()
+		return json.loads(response)
 
 def make_panel(h,l, y,x, str):
 	win = curses.newwin(h,l, y,x)
@@ -36,8 +51,6 @@ def make_panel(h,l, y,x, str):
 
 def test(stdscr):
 
-	WEATHER_WAIT = 60*60
-	WEATHER_URL = 'https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20weather.forecast%20where%20woeid%20in%20(select%20woeid%20from%20geo.places(1)%20where%20text%3D%22union%20city%2C%20nj%22)&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys'
 	try:
 		curses.curs_set(0)
 	except:
@@ -48,15 +61,14 @@ def test(stdscr):
 	time_win, time_panel = make_panel(5,23, 5,5, "Time")
 	weather_win, weather_panel = make_panel(8,70, 10,5, "Weather")
 
-	fetcher = CacheFetcher()
+	weather = Weather(city='union city, nj', metric=False)
 
 	while True:
-		foo = fetcher.fetch(WEATHER_URL, WEATHER_WAIT).decode()
-		loo = json.loads(foo)
 		time_win.addstr(3,2,time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime()))
+		w = weather.get()
 
 		x = 2
-		for day in loo['query']['results']['channel']['item']['forecast']:
+		for day in w['query']['results']['channel']['item']['forecast']:
 			weather_win.addstr(4,x,day['high'])
 			weather_win.addstr(5,x,day['low'])
 
