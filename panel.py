f36eef531f420ef222dbc5fc01d927cefe19dd1f
Chris Barry <chris@barry.im>
2015-09-19 16:32:30 -0400

    Making slightly more generic, removed time pane put into main title.

--- panel.py
+++ panel.py
@@ -16,71 +16,97 @@ class CacheFetcher:
 		self.cache = {}
 
 	def fetch(self, url, max_age=0):
-		#if self.cache.has_key(url):
 		if url in self.cache:
 			if int(time.time()) - self.cache[url][0] < max_age:
 				return self.cache[url][1]
+
 		# Retrieve and cache
-		data = urllib.request.urlopen(url).read()
+		try:
+			data = urllib.request.urlopen(url).read()
+		except IOError:
+			# Return last cache if we have it
+			if url in self.cache:
+				return self.cache[url][1]
+			else:
+				data = None
+
+		# PUT TRY AROUND THIS
 		self.cache[url] = (time.time(), data)
 		return data
 
 # Holds configuration and code for fetching weather
 class Weather:
-	def __init__(self, city='', metric=False, cache=60*60):
+	def __init__(self, city='', metric=False, cache_time=60*60):
+		self.unit = 'f'
 		if metric:
-			u = 'c'
-		else:
-			u = 'f'
-		self.WEATHER_WAIT = cache
-		self.WEATHER_URL = 'https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20weather.forecast%20where%20woeid%20in%20(select%20woeid%20from%20geo.places(1)%20where%20text%3D%22{}%22)%20and%20u%3D%22{}%22&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys'.format(urllib.parse.quote(city), u)
+			self.unit = 'c'
+		self.CACHE_TIME = cache_time
+		self.API = 'https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20weather.forecast%20where%20woeid%20in%20(select%20woeid%20from%20geo.places(1)%20where%20text%3D%22{}%22)%20and%20u%3D%22{}%22&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys'.format(urllib.parse.quote(city), self.unit)
 		self.fetcher = CacheFetcher()
 
 	def get(self):
-		response = self.fetcher.fetch(self.WEATHER_URL, self.WEATHER_WAIT).decode()
+		response = self.fetcher.fetch(self.API, self.CACHE_TIME).decode()
 		return json.loads(response)
 
 def make_panel(h,l, y,x, str):
 	win = curses.newwin(h,l, y,x)
 	win.erase()
 	win.box()
-	win.addstr(1, 2, str, curses.A_DIM|curses.A_UNDERLINE)
+	win.addstr(1, 2, str, curses.A_UNDERLINE)
 
 	panel = curses.panel.new_panel(win)
 	return win, panel
 
 def test(stdscr):
 
+	# Seconds
+	REFRESH_DELAY = 10
+	#REFRESH_DELAY = 60
+	TITLE = "Things Today"
+	WEATHER_TITLE = "Weather"
+	TRANSIT_TITLE = "Transit"
+
+	WEATHER_CITY = "Union City, NJ"
+
 	try:
 		curses.curs_set(0)
 	except:
 		pass
 	stdscr.box()
-	stdscr.addstr(2, 2, "Union City Now", curses.A_BOLD)
+	stdscr.addstr(1, 1, TITLE, curses.A_BOLD)
 
-	time_win, time_panel = make_panel(5,23, 5,5, "Time")
-	weather_win, weather_panel = make_panel(8,70, 10,5, "Weather")
+	# Panels
+	weather_win, weather_panel = make_panel(8,75, 3,2, WEATHER_TITLE)
 
-	weather = Weather(city='union city, nj', metric=False)
+	# APIs and stuff
+	weather = Weather(city=WEATHER_CITY, metric=False)
 
 	while True:
-		time_win.addstr(3,2,time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime()))
+		# ----- BEGIN TIME -----
+		stdscr.addstr(1, len(TITLE)+1,time.strftime(" - %Y-%m-%d %H:%M",))
+		# ----- END TIME -----
+
+		# ----- BEGIN WEATHER -----
 		w = weather.get()
+		u = weather.unit.upper()
 
 		x = 2
+		width = 16
 		for day in w['query']['results']['channel']['item']['forecast']:
-			weather_win.addstr(4,x,day['high'])
-			weather_win.addstr(5,x,day['low'])
-
 			weather_win.addstr(3,x,day['day'])
+			weather_win.addstr(4,x,'{}{}'.format(day['high'],u))
+			weather_win.addstr(5,x,'{}{}'.format(day['low'],u))
+			weather_win.addstr(6,x,day['text'][:width-1])
+			x = x + width
+		weather_win.addstr(1,len(WEATHER_TITLE)+3,'- '+w['query']['results']['channel']['title'], curses.A_DIM)
+		# ----- END WEATHER -----
 
-			weather_win.addstr(4,x+3,day['text'][:6])
-
-			x = x + 13
+		# ----- BEGIN TRANSIT -----
+		# ----- END TRANSIT -----
 
 		curses.panel.update_panels();
 		stdscr.refresh()
-		time.sleep(1)
+		time.sleep(REFRESH_DELAY)
 
 
 if __name__ == '__main__':
